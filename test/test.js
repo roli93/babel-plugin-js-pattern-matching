0dbd40497ffe2854749ff96aa351974b2968788a
Rodolfo Caputo <rodcap.93@gmail.com>
2017-03-12 21:47:23 -0300

    Adding basic project structure

---
+++ test/test.js
@@ -0,0 +1,14 @@
+import expect from 'expect.js';
+
+describe('JS Pattern Matching Plugin', function() {
+
+  context('Blah', () => {
+
+    it('should bleh', () => {
+      expect("X").to.equal("X");
+    });
+
+  });
+
+
+});
